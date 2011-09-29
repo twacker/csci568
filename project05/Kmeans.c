54dd9c723d024f1ba0892886b54adbf10cacb1c7
Taylor Wacker <twacker@mymail.mines.edu>
2011-09-28 19:10:22 -0600

    got it working

--- project05/Kmeans.c
+++ project05/Kmeans.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <iostream>
 #include <math.h>
 
 double Euclidean(double a[], double b[], int size){
@@ -15,7 +14,7 @@ double Euclidean(double a[], double b[], int size){
 }
 
 double* Kmeans(double data[][150], int k, int datapoints, int count, double groups[]){
-	int i;
+	int i, j, l;
 	for(i=0; i<=count;i ++){
 		groups[i] = 0;
 	}
@@ -26,16 +25,63 @@ double* Kmeans(double data[][150], int k, int datapoints, int count, double grou
 		}
 		return groups;
 	}
-	double k_points[datapoints][k];
+	//initalize the kpoint
+	// datapoints is 4 for iris dataset
+	double k_points[datapoints+1][k];
+	srand((unsigned)time(NULL));
 	for (i = 0; i < k ; i ++){
-		int j;
 		for( j = 0 ; j < datapoints; j ++){
-			k_points[i][j] = data[i][j];
+			l = rand()%count;
+			k_points[j][i] = data[j][l];
 		}
 	}
-
-
-
+	int rounds;
+	for(rounds = 0; rounds < 100; rounds ++){
+		// find which group each point belongs
+		for(i = 0; i < count; i ++){
+			double min_length = 10000;
+			int k_val=0;
+			for( j = 0; j < k; j ++){
+				double a[datapoints], b[datapoints];
+				for(l = 0; l < datapoints;l++){
+					a[l] = data[l][i];
+					b[l] = k_points[l][j];
+				} 
+				double length = Euclidean(a,b,4);
+				if(length < min_length){
+					min_length = length;
+					k_val = j;
+				}
+			}
+			data[datapoints+1][i] = k_val;	
+		}
+		// clear the datapoints in k_points
+		for(i = 0; i < k ; i ++){
+			for(j = 0 ; j <= datapoints; j++){
+				k_points[j][i] = 0;
+			}
+		}
+		//generate new k_points
+		//add up all values assiciated with point k
+		for(i = 0; i < count; i++){
+			j = data[datapoints+1][i];
+			for( l = 0; l < datapoints; l ++){
+				k_points[l][j] += data[l][i];
+			}
+			k_points[datapoints][j] += 1;
+		}
+		//devide by the number of points
+		for(j = 0; j < k; j++){
+			for( l = 0; l < datapoints; l ++){
+				if(k_points[datapoints][j] >= 1){k_points[l][j] /= k_points[datapoints][j];}
+			}
+		}
+		//start over again
+	}
+	// translate into groups
+	for(i = 0; i < count; i ++){
+		groups[i] = data[datapoints +1][i];
+	}
 	return groups;	
 }
 
@@ -55,7 +101,12 @@ void test(){
 	fclose(file);
 	double temp[150];
 	double * groups = Kmeans(data,3,4,count,temp);
-
+	int i;
+	for (i = 0 ; i < 150; i ++){
+		printf("plant %d is in group %d\n", i ,(int) groups[i]);
+	}
+	printf("These are the results for my k-means clustering algorithm\nusing the iris dataset looking for 3 groups\ngroups are 0,1, and 2");
+	return;
 	
 	
 }
